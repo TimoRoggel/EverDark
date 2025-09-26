9f7327ad6f98696803863aa3aa070f4fcdfd6214
AmanTrechsel <30940054+Voltseon@users.noreply.github.com>
2025-09-26 10:24:56 +0200

    finite state machine & component icons

---
+++ scripts/state_machine/player_states/player_idle_state.gd
@@ -0,0 +1,19 @@
+class_name PlayerIdleState extends State
+
+## Called when the state is entered.
+func _enter(_enter_parameter: Variant = null) -> void:
+	pass
+
+## Called when the state is exited.
+func _exit(_exit_parameter: Variant = null) -> void:
+	pass
+
+## Called every physics update.
+func _update(_delta: float) -> void:
+	# Don't update if no input is initialized.
+	if !_controller().input:
+		return
+	# If moving transition to walk.
+	if _controller().input.movement.length() >= 0.1:
+		# Change state.
+		transition.emit(self, "walk")

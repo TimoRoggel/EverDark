54815213ba08c56355611e1ee0563095e1941523
Levon <57988594+Levon7@users.noreply.github.com>
2025-09-26 16:07:42 +0200

    Edit hitbox component
    - connected on body entered signal
    - added cooldown

--- scripts/components/hitbox_component.gd
+++ scripts/components/hitbox_component.gd
@@ -1,9 +1,18 @@
 class_name HitboxComponent extends Component
 
+@export var hitbox_area : Area2D
 @export var health_component : HealthComponent
+@export var damage : int = 10
+@export var is_active: bool = true
+@export var attack_cooldown: float = 1.0
+
+var already_hit = false
 
 func _enter() -> void:	
-	health_component.damage_taken.connect(damage)
+	#health_component.damage_taken.connect(damage)
+	if hitbox_area:
+		hitbox_area.body_entered.connect(_on_body_entered)
+	pass
 
 func _update(_delta: float) -> void:
 	pass
@@ -11,6 +20,18 @@ func _update(_delta: float) -> void:
 func _exit() -> void:
 	pass
 
-func damage(from):
+func _on_body_entered(body: Node2D):
+	if not is_active:
+		return
+	if !already_hit && body.get_node("components/health") && body is ProjectileController:
+		take_damage(body)
+		cooldown()
+		
+func take_damage(from):
 	if health_component:
 		health_component.take_damage(from as ProjectileController)
+		
+func cooldown():
+	already_hit = true
+	await get_tree().create_timer(attack_cooldown).timeout
+	already_hit = false

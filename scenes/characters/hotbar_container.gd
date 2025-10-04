9d7f7908fa58f2f76a59cf4a0c8651239ea2b1d5
Levon <57988594+Levon7@users.noreply.github.com>
2025-10-04 10:59:06 +0200

    Edit hotbar and cleanup
    - added updating currently selected slot
    - added keyinput for troping items
    - moved creating nodes for slots into seperate functions

--- scenes/characters/hotbar_container.gd
+++ scenes/characters/hotbar_container.gd
@@ -3,45 +3,62 @@ extends HBoxContainer
 @export var inventory: InventoryContainer
 
 var slots_per_row: int
-
 var currently_selected_slot: int = 0
+var is_active = true
 
 func _ready() -> void:
 	if inventory:
 		slots_per_row = inventory.slots/inventory.rows
 		
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("select_left"):
-		print(inventory.get_slots()[0].inventory_item)
+	if event.is_action_pressed("drop_item") and is_active:
+		var current_item = inventory.get_slots()[currently_selected_slot].inventory_item
+		if current_item:
+			print("Dropped: ", inventory.get_slots()[currently_selected_slot].inventory_item)
+		else:
+			print("Empty slot")
 	
 func _process(delta: float) -> void:
 	add_slots()
 	update_hotbar()
+	update_currently_selected_slot()
 	
 func add_slots():
 	if inventory && self.get_child_count() == 0:
 		for i in slots_per_row:
-			var slot_texture = TextureButton.new()
-			slot_texture.texture_normal = preload("res://graphics/ui_icons/hotbar_slot_normal.png")
-			slot_texture.texture_focused = preload("res://graphics/ui_icons/hotbar_slot_focus.png")
-			var item_texture = TextureRect.new()
-			item_texture.stretch_mode = TextureRect.STRETCH_KEEP
-			item_texture.anchor_left = 0.0
-			item_texture.anchor_top = 0.0
-			item_texture.anchor_right = .8
-			item_texture.anchor_bottom = .8
-			var amount_label = Label.new()
-			amount_label.text = str(0) + "x"
-			amount_label.anchor_left = 0.0
-			amount_label.anchor_top = 0.0
-			amount_label.anchor_right = 1.0
-			amount_label.anchor_bottom = 1.0
-			amount_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
-			amount_label.vertical_alignment = VERTICAL_ALIGNMENT_BOTTOM
+			var slot_texture = create_slot()
+			var item_texture = create_item_texture()
+			var amount_label = create_amount_label()
 			slot_texture.add_child(item_texture)
 			slot_texture.add_child(amount_label)
 			self.add_child(slot_texture)
 		select_slot(currently_selected_slot)
+		
+func create_slot() -> TextureButton:
+	var slot_texture = TextureButton.new()
+	slot_texture.texture_normal = preload("res://graphics/ui_icons/hotbar_slot_normal.png")
+	slot_texture.texture_focused = preload("res://graphics/ui_icons/hotbar_slot_focus.png")
+	return slot_texture
+	
+func create_item_texture() -> TextureRect:
+	var item_texture = TextureRect.new()
+	item_texture.stretch_mode = TextureRect.STRETCH_KEEP
+	item_texture.anchor_left = 0.0
+	item_texture.anchor_top = 0.0
+	item_texture.anchor_right = .8
+	item_texture.anchor_bottom = .8
+	return item_texture
+	
+func create_amount_label() -> Label:
+	var amount_label = Label.new()
+	amount_label.text = str(0) + "x"
+	amount_label.anchor_left = 0.0
+	amount_label.anchor_top = 0.0
+	amount_label.anchor_right = 1.0
+	amount_label.anchor_bottom = 1.0
+	amount_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+	amount_label.vertical_alignment = VERTICAL_ALIGNMENT_BOTTOM
+	return amount_label
 		
 func update_hotbar():
 	if inventory:
@@ -67,3 +84,10 @@ func scale_texture_rect(texture_rect: TextureRect, parent_size: Vector2):
 	var tex_size = texture_rect.texture.get_size()
 	var scale = min(parent_size.x / tex_size.x, parent_size.y / tex_size.y)
 	texture_rect.scale = Vector2(scale, scale)
+	
+func update_currently_selected_slot():
+	for child in self.get_children():
+		if child.has_focus():
+			if currently_selected_slot != child.get_index():
+				currently_selected_slot = child.get_index()
+			break

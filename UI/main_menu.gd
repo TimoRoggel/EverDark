e2721d595c67cb17835926305e5cd01144e70264
oleksiisapelnykov <oleksiisapelnykov@icloud.com>
2025-10-14 17:33:14 +0200

    123

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,15 +1,18 @@
-class_name MainMenu extends Control
-@onready var start: Button = $MarginContainer/HBoxContainer/VBoxContainer/Start
-@onready var options: Button = $MarginContainer/HBoxContainer/VBoxContainer/Options
-@onready var exit: Button = $MarginContainer/HBoxContainer/VBoxContainer/Exit
+class_name MainMenu  extends Control
+@onready var start: Button = $HBoxContainer/VBoxContainer/Start
+@onready var achievements: Button = $HBoxContainer/VBoxContainer/Achievements
+@onready var settings: Button = $HBoxContainer/VBoxContainer/Settings
+@onready var exit: Button = $HBoxContainer/VBoxContainer/Exit
 @onready var start_level = preload("res://scenes/levels/world.tscn") as PackedScene
-
-
-func _on_options_pressed() -> void:
-	pass
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
 	
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_settings_pressed() -> void:
+	pass 
+
+func _on_achievements_pressed() -> void:
+	pass 

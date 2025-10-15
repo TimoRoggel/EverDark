9cdfef53e8434d0b2a9ec25a767bccf14ebd0587
oleksiisapelnykov <oleksiisapelnykov@icloud.com>
2025-10-15 19:51:09 +0200

    test

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -6,6 +6,7 @@ class_name MainMenu  extends Control
 @onready var exit: Button = $MarginContainer/HBoxContainer/VBoxContainer/Exit
 @onready var start_level = preload("res://scenes/levels/world.tscn") as PackedScene
 @onready var settings_menu: Settings_Menu = $SettingsMenu as Settings_Menu
+@onready var margin_container_2: MarginContainer = $MarginContainer2
 
 
 func _on_start_pressed() -> void:
@@ -15,6 +16,7 @@ func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 func _on_settings_pressed() -> void:
+	margin_container_2.visible = false
 	margin_container.visible = false
 	settings_menu.set_process(true)
 	settings_menu.visible = true 
@@ -24,5 +26,6 @@ func _on_achievements_pressed() -> void:
 
 
 func _on_settings_menu_exit_settings_menu() -> void:
+	margin_container_2.visible = true
 	margin_container.visible = true
 	settings_menu.visible = false 
--- UI/pause_menu.gd
+++ UI/pause_menu.gd
@@ -1,18 +1,15 @@
 class_name PauseMenu extends Control
-@onready var options: Panel = $Options
+@onready var settings_menu: Settings_Menu = $SettingsMenu
 @onready var buttons: VBoxContainer = $PauseMenu/Buttons
-@onready var control_panel: Panel = $"Control panel"
-@onready var audio_panel: Panel = $"Audio panel"
-@onready var graphics_panel: Panel = $"Graphics panel"
+
 var paused: bool = false
 
 func _ready() -> void:
 	visible = false
 	get_tree().paused = false
 	buttons.visible = true
-	options.visible = false 
-	graphics_panel.visible = false 
-	audio_panel.visible = false 
+	settings_menu.visible = false 
+	
 
 
 func _process(_delta: float) -> void:
@@ -43,38 +40,4 @@ func _on_resume_pressed() -> void:
 
 func _on_options_pressed() -> void:
 	buttons.visible = false
-	options.visible = true 
-
-func _on_back_pressed() -> void:
-	buttons.visible = true
-	options.visible = false 
-
-func _on_controls_pressed() -> void:
-	buttons.visible = false
-	options.visible = false
-	graphics_panel.visible = false 
-	audio_panel.visible = false 
-	control_panel.visible = true 
-	
-
-func _on_graphics_pressed() -> void:
-	buttons.visible = false
-	options.visible = false
-	graphics_panel.visible = true 
-	audio_panel.visible = false 
-	control_panel.visible = false  
-
-
-func _on_audio_pressed() -> void:
-	buttons.visible = false
-	options.visible = false
-	graphics_panel.visible = false 
-	audio_panel.visible = true 
-	control_panel.visible = false  
-	
-func _on_back_c_pressed() -> void:
-	buttons.visible = false
-	options.visible = true 
-	graphics_panel.visible = false 
-	audio_panel.visible = false 
-	control_panel.visible = false 
+	settings_menu.visible = true 

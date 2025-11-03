44572ace980cdfdea299d75f86e9fc09f26a4175
oleksiisapelnykov <oleksiisapelnykov@icloud.com>
2025-11-03 18:08:34 +0100

    added fading transition

--- UI/Pause Menu/pause_menu.gd
+++ UI/Pause Menu/pause_menu.gd
@@ -43,12 +43,11 @@ func _on_options_pressed() -> void:
 	buttons.visible = false
 	settings_pause_menu.visible = true 
 
-
 func _on_settings_pause_menu_exit_settings_pause_menu() -> void:
 	buttons.visible = true
 	settings_pause_menu.visible = false 
 
-
 func _on_back_to_main_menu_pressed() -> void:
 	get_tree().paused = false  
-	get_tree().change_scene_to_file("res://UI/Main menu/main_menu.tscn")
+	var target_scene_path = "res://UI/Main menu/main_menu.tscn"
+	SceneTransitionController.change_scene(target_scene_path, "fade_layer", 1.0)

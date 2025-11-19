adfeefb3862499b5fdda901e5128adfa0b823a80
Voltseon <30940054+Voltseon@users.noreply.github.com>
2025-11-19 18:17:59 +0100

    highlight interactables

--- scripts/tools/interactable_2d.gd
+++ scripts/tools/interactable_2d.gd
@@ -1,6 +1,8 @@
 @tool
 class_name Interactable2D extends Area2D
 
+const SHADER: Shader = preload("uid://b0wqu3tlyguu1")
+
 @export var radius: float = 8.0:
 	set(value):
 		radius = max(0.0001, value)
@@ -17,16 +19,22 @@ var interact_player: AudioStreamPlayer2D = AudioStreamPlayer2D.new()
 var active: bool = true
 var instance: Runnable = null
 
+signal interactable
+signal not_interactable
+
 func _ready() -> void:
-	monitoring = false
+	monitoring = true
+	material = _create_material()
 	collision_layer = 4
-	collision_mask = 0
+	collision_mask = 4
 	shape.radius = radius
 	collision.shape = shape
 	add_child(collision)
 	interact_player.stream = interact_sound
 	add_child(interact_player)
 	instance = interact_script.new()
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
 
 func merge_param(controller: CharacterController) -> Dictionary:
 	var p_exp: Expression = Expression.new()
@@ -35,10 +43,14 @@ func merge_param(controller: CharacterController) -> Dictionary:
 
 func can_interact(controller: CharacterController) -> bool:
 	if !is_visible_in_tree():
+		set_interactable(false)
 		return false
 	if !active:
+		set_interactable(false)
 		return false
-	return instance.can_run(merge_param(controller))
+	var can: bool = instance.can_run(merge_param(controller))
+	set_interactable(can)
+	return can
 
 func interact(controller: CharacterController) -> void:
 	if !active:
@@ -46,6 +58,7 @@ func interact(controller: CharacterController) -> void:
 	if interact_sound:
 		interact_player.play()
 	instance.run(merge_param(controller))
+	can_interact(controller)
 
 func set_active(timeout: float) -> void:
 	active = false
@@ -65,3 +78,29 @@ func _notification(what: int) -> void:
 			queue_redraw()
 		NOTIFICATION_TRANSFORM_CHANGED:
 			queue_redraw()
+
+func _create_material() -> ShaderMaterial:
+	var mat: ShaderMaterial = ShaderMaterial.new()
+	mat.shader = SHADER
+	return mat
+
+func _on_area_entered(area: Area2D) -> void:
+	var component: Node = area.get_parent()
+	if !is_instance_of(component, InteractionComponent):
+		return
+	if !can_interact(component.controller):
+		return
+	pass
+
+func _on_area_exited(area: Area2D) -> void:
+	var component: Node = area.get_parent()
+	if !is_instance_of(component, InteractionComponent):
+		return
+	set_interactable(false)
+
+func set_interactable(toggled: bool) -> void:
+	material.set_shader_parameter("highlighted", toggled)
+	if toggled:
+		interactable.emit()
+	else:
+		not_interactable.emit()

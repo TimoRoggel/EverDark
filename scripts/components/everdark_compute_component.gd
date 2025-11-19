f16e6bb92d909321fb291d057b10fe616fb946cd
Voltseon <30940054+Voltseon@users.noreply.github.com>
2025-11-19 17:48:40 +0100

    start on dynamic lumin sizes

--- scripts/components/everdark_compute_component.gd
+++ scripts/components/everdark_compute_component.gd
@@ -13,20 +13,36 @@ func _enter() -> void:
 	updates_in_physics = false
 
 func _update(_delta: float) -> void:
-	everdark_material.set_shader_parameter("lumin_positions", get_local_lumin_positions())
+	var lumin_transforms: Array = get_lumin_transforms()
+	everdark_material.set_shader_parameter("lumin_positions", lumin_transforms.map(func(t: Array) -> Vector2: return t[0]))
+	#everdark_material.set_shader_parameter("lumin_sizes", lumin_transforms.map(func(t: Array) -> float: return t[1]))
 	everdark_material.set_shader_parameter("lumin_count", min(64, Generator.lumin_positions.size()))
 	everdark_material.set_shader_parameter("player_position", Debug.to_screen(global_position) + OFFSET)
 
 func _exit() -> void:
 	pass
 
-func get_local_lumin_positions() -> PackedVector2Array:
+func get_lumin_transforms() -> Array:
+	var transforms: Array = []
 	var positions: Array = []
 	positions.append_array(Generator.lumin_positions)
-	positions.sort_custom(func(a: Vector2, b: Vector2) -> bool:
-		return a.distance_squared_to(camera.global_position) < b.distance_squared_to(camera.global_position)
+	var sizes: PackedFloat32Array = []
+	sizes.append(Generator.LUMIN_SIZE * 2.0)
+	
+	for i: int in positions.size():
+		var pos: Vector2 = positions[i]
+		var size: float = Generator.LUMIN_SIZE
+		if sizes.size() > i:
+			size = sizes[i]
+		transforms.append([pos, size])
+	
+	transforms.sort_custom(func(a: Array, b: Array) -> bool:
+		return a[0].distance_squared_to(camera.global_position) < b[0].distance_squared_to(camera.global_position)
 	)
-	positions = positions.map(func(pos: Vector2) -> Vector2: return Debug.to_screen(pos) + OFFSET)
-	var shader_positions: PackedVector2Array = PackedVector2Array(positions)
-	shader_positions.resize(64)
-	return shader_positions
+	transforms = transforms.map(func(a: Array) -> Array: return [Debug.to_screen(a[0]) + OFFSET, a[1]])
+
+	while transforms.size() < 64:
+		transforms.append([Vector2.ZERO, 0.0])
+	transforms.resize(64)
+	
+	return transforms

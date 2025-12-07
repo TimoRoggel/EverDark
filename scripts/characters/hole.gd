b9b7678b9f4aa4ee2eef8df307483867aa48d52d
Voltseon <30940054+Voltseon@users.noreply.github.com>
2025-12-07 21:26:38 +0100

    added hole art

--- scripts/characters/hole.gd
+++ scripts/characters/hole.gd
@@ -1,9 +1,35 @@
-extends Node2D
+extends AnimatedSprite2D
 
 @onready var convert_sound: AudioStreamPlayer2D = %convert_sound
+@onready var area: Area2D = %area
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	if is_instance_of(area, DroppedItem2D) && area.item.id == 1:
-		area.item = DataManager.get_resource_by_id("items", 0)
-		convert_sound.play()
-		GameManager.finish_objective(1)
+var convert_speed: float = 0.5
+
+func _physics_process(_delta: float) -> void:
+	if animation == &"bring_up":
+		return
+	var has_cores: bool = false
+	for a: Area2D in area.get_overlapping_areas():
+		if is_instance_of(a, DroppedItem2D) && a.item.id == 1:
+			has_cores = true
+	if !has_cores:
+		return
+	play(&"bring_up")
+	for a: Area2D in area.get_overlapping_areas():
+		_on_area_2d_area_entered(a)
+	await get_tree().create_timer(convert_speed, false).timeout
+	convert_sound.play()
+	GameManager.finish_objective(1)
+	await animation_finished
+	play(&"default")
+
+func _on_area_2d_area_entered(a: Area2D) -> void:
+	if !is_instance_of(a, DroppedItem2D):
+		return
+	if a.item.id != 1:
+		return
+	a.visible = false
+	a.set_active(convert_speed)
+	a.item = DataManager.get_resource_by_id("items", 0)
+	await get_tree().create_timer(convert_speed, false).timeout
+	a.visible = true

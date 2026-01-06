19e8976e673a48c5897bfa703d0f277c217eaff3
oleksiisapelnykov <oleksiisapelnykov@icloud.com>
2026-01-06 21:17:38 +0100

    started working on Journal

---
+++ UI/journal_menu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+@onready var menu_layer = $BookBackground/Menu_Layer
+@onready var tutorial_layer = $BookBackground/Tutorial_Layer
+@onready var notes_layer = $BookBackground/Notes_layer
+
+func _ready():
+	show_menu()
+
+func show_menu():
+	menu_layer.visible = true
+	tutorial_layer.visible = false
+	notes_layer.visible = false
+
+func _on_btn_tutorial_pressed():
+	menu_layer.visible = false
+	tutorial_layer.visible = true
+	notes_layer.visible = false
+
+func _on_btn_notes_pressed():
+	menu_layer.visible = false
+	tutorial_layer.visible = false
+	notes_layer.visible = true
+
+func _on_btn_back_to_menu_pressed():
+	show_menu()
+	
+
+func _on_button_pressed() -> void:
+	visible = false
